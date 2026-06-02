5e1d7d64e25f0e2c4390ee8d7180d1563fa6a36e
ketana0224 <ketana@microsoft.com>
2026-06-02 12:38:25 +0900

    chore: remove unused scripts

--- scripts/run-bedrock-sdk.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-$env:APIM_BEDROCK_URL = "https://apim-aigw-userxx.azure-api.net/bedrock"
-$env:APIM_SUBSCRIPTION_KEY = az rest --method post --uri "https://management.azure.com/subscriptions/9353f1a1-94a4-4e4b-ae82-c27ea3d07160/resourceGroups/rg-aigw-handson-userxx/providers/Microsoft.ApiManagement/service/apim-aigw-userxx/subscriptions/sub-aigw-handson/listSecrets?api-version=2022-08-01" --query "primaryKey" -o tsv
-Push-Location .\bedrock
-dotnet run --nologo
-Pop-Location
